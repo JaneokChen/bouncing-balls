c2ba73e3e84b49815267c752c9021a217e8a7b38
Wanttofeedacat <247345858@qq.com>
2021-04-24 20:55:17 +0800

    first commit

--- todolist/todolist.js
+++
@@ -1,8 +0,0 @@
-let textBox = document.getElementById('textBox');
-let text = textBox.value;
-let submit = document.getElementById('submit');
-
-
-submit.addEventListener('click', (event)=>{
-  alert(text);
-})
